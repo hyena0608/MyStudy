2945d84181896810d9233bf04c97378744b3aefb
hyena0608 <goatsfish@gmail.com>
2022-04-24 10:36:10 +0900

    [Vue] 싱글 파일 컴포넌트에서 props 사용해보기

--- Vue-JS/vue-cli/src/App.vue
+++ Vue-JS/vue-cli/src/App.vue
@@ -1,34 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <!-- 컴포넌트 명명법 종류 -->
-    <!-- <hello-world></hello-world>
-    <HelloWorld></HelloWorld>
-    <HelloWorld/> -->
-    
+  <div>
+    <app-header v-bind:propsdata="str"></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppHeader from './components/AppHeader.vue';
 export default {
-  // 인스턴스 옵션 속석 or 컴포넌트 옵션 속성
-  name: 'App',
+  data: function() {
+    return {
+      str: 'Header'
+    }
+  },
   components: {
-    HelloWorld
+    'app-header': AppHeader
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
