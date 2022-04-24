d97b6ed44ecef99966facb6634e95c89964ac7a2
hyena0608 <goatsfish@gmail.com>
2022-04-24 16:22:14 +0900

    [Vue] todolist - refactor: props 사용해서 할 일 목록 표시 기능 구현

--- Vue-JS/중급/vue-todo/src/App.vue
+++ Vue-JS/중급/vue-todo/src/App.vue
@@ -2,25 +2,41 @@
   <div>
     <TodoHeader></TodoHeader>
     <TodoInput></TodoInput>
-    <TodoList></TodoList>
+    <TodoList v-bind:propsdata="todoItems"></TodoList>
     <TodoFooter></TodoFooter>
   </div>
 </template>
 
 <script>
-import TodoHeader from './components/TodoHeader.vue';
-import TodoInput from './components/TodoInput.vue';
-import TodoList from './components/TodoList.vue';
-import TodoFooter from './components/TodoFooter.vue';
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoFooter from "./components/TodoFooter.vue";
 
 export default {
+  data: function () {
+    return {
+      todoItems: [],
+    }
+  },
+  created: function () {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(
+            JSON.parse(localStorage.getItem(localStorage.key(i)))
+          );
+        }
+      }
+    }
+  },
   components: {
-    'TodoHeader': TodoHeader,
-    'TodoList': TodoList,
-    'TodoInput': TodoInput,
-    'TodoFooter': TodoFooter
-  }
-}
+    TodoHeader: TodoHeader,
+    TodoList: TodoList,
+    TodoInput: TodoInput,
+    TodoFooter: TodoFooter,
+  },
+};
 </script>
 
 <style>
--- Vue-JS/중급/vue-todo/src/components/TodoList.vue
+++ Vue-JS/중급/vue-todo/src/components/TodoList.vue
@@ -2,12 +2,16 @@
   <div>
     <ul>
       <li
-        v-for="(todoItem, index) in todoItems"
+        v-for="(todoItem, index) in propsdata"
         v-bind:key="todoItem.item"
         class="shadow"
       >
-        <i class="checkBtn fa-solid fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
-        <span v-bind:class="{textCompleted: todoItem.completed}">
+        <i
+          class="checkBtn fa-solid fa-check"
+          v-bind:class="{ checkBtnCompleted: todoItem.completed }"
+          v-on:click="toggleComplete(todoItem, index)"
+        ></i>
+        <span v-bind:class="{ textCompleted: todoItem.completed }">
           {{ todoItem.item }}
         </span>
         <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
@@ -20,33 +24,18 @@
 
 <script>
 export default {
-  data: function () {
-    return {
-      todoItems: [],
-    };
-  },
+  props: ['propsdata'],
   methods: {
     removeTodo: function (todoItem, index) {
       localStorage.removeItem(todoItem);
       this.todoItems.splice(index, 1);
     },
     toggleComplete: function (todoItem, index) {
-        todoItem.completed = !todoItem.completed;
-        // 로컬 스토리지 데이터 갱신
-        localStorage.removeItem(todoItem.item);
-        localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+      todoItem.completed = !todoItem.completed;
+      // 로컬 스토리지 데이터 갱신
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     },
-  },
-  created: function () {
-    if (localStorage.length > 0) {
-      for (var i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
-          this.todoItems.push(
-            JSON.parse(localStorage.getItem(localStorage.key(i)))
-          );
-        }
-      }
-    }
   },
 };
 </script>
