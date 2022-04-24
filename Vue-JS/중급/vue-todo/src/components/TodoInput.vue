453ddf259b65c4f3963dd5e89653deb035e42cdc
hyena0608 <goatsfish@gmail.com>
2022-04-24 15:19:59 +0900

    [Vue] todolist - feat: 컴포넌트 할 일 완료 기능 구현

--- Vue-JS/중급/vue-todo/src/components/TodoInput.vue
+++ Vue-JS/중급/vue-todo/src/components/TodoInput.vue
@@ -2,7 +2,7 @@
   <div class="inputBox shadow">
     <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
     <span class="addContainer" v-on:click="addTodo">
-        <i class="fa-solid fa-plus addBtn"></i>
+      <i class="fa-solid fa-plus addBtn"></i>
     </span>
   </div>
 </template>
@@ -17,11 +17,14 @@ export default {
   methods: {
     addTodo: function () {
       // 저장하는 로직
-      localStorage.setItem(this.newTodoItem, this.newTodoItem);
-      this.clearInput();
+      if (this.newTodoItem !== "") {
+        var obj = { completed: false, item: this.newTodoItem };
+        localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+        this.clearInput();
+      }
     },
-    clearInput: function() {
-        this.newTodoItem = '';
+    clearInput: function () {
+      this.newTodoItem = "";
     },
   },
 };
@@ -29,31 +32,31 @@ export default {
 
 <style scoped>
 input:focus {
-    outline:none;
+  outline: none;
 }
 
 .inputBox {
-    background: white;
-    height: 50px;
-    line-height: 50px;
-    border-radius: 5px;
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
 }
 
 .inputBox input {
-    border-style: none;
-    font-size: 0.9rem;
+  border-style: none;
+  font-size: 0.9rem;
 }
 
 .addContainer {
-    float: right;
-    background: linear-gradient(to right, #6478FB, #8763FB);
-    display: block;
-    width: 3rem;
-    border-radius: 0 5px 5px 0;
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
 }
 
 .addBtn {
-    color: white;
-    vertical-align: middle;
+  color: white;
+  vertical-align: middle;
 }
 </style>
--- Vue-JS/중급/vue-todo/src/components/TodoList.vue
+++ Vue-JS/중급/vue-todo/src/components/TodoList.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
     <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
-        {{ todoItem }}
+      <li
+        v-for="(todoItem, index) in todoItems"
+        v-bind:key="todoItem.item"
+        class="shadow"
+      >
+        <i class="checkBtn fa-solid fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
+        <span v-bind:class="{textCompleted: todoItem.completed}">
+          {{ todoItem.item }}
+        </span>
         <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
-            <i class="fa-solid fa-trash-can"></i>
+          <i class="fa-solid fa-trash-can"></i>
         </span>
       </li>
     </ul>
@@ -19,16 +26,24 @@ export default {
     };
   },
   methods: {
-      removeTodo: function(todoItem, index) {
-          localStorage.removeItem(todoItem);
-          this.todoItems.splice(index, 1);
-      }
+    removeTodo: function (todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    },
+    toggleComplete: function (todoItem, index) {
+        todoItem.completed = !todoItem.completed;
+        // 로컬 스토리지 데이터 갱신
+        localStorage.removeItem(todoItem.item);
+        localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
   },
   created: function () {
     if (localStorage.length > 0) {
       for (var i = 0; i < localStorage.length; i++) {
         if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
-          this.todoItems.push(localStorage.key(i));
+          this.todoItems.push(
+            JSON.parse(localStorage.getItem(localStorage.key(i)))
+          );
         }
       }
     }
@@ -36,42 +51,42 @@ export default {
 };
 </script>
 
-<style>
-    ul {
-        list-style-type: none;
-        padding-left: 0px;
-        margin-top: 0;
-        text-align: left;
-    }
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
 
-    li {
-        display: flex;
-        min-height: 50px;
-        height: 50px;
-        line-height: 50px;
-        margin: 0.5rem 0;
-        padding: 0 0.9rem;
-        background: white;
-        border-radius: 5px;
-    }
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
 
-    .checkBtn {
-        line-height: 45px;
-        color: #62acde;
-        margin-right: 5px;
-    }
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
 
-    .checkBtnCompleted {
-        color: #b3adad;
-    }
+.checkBtnCompleted {
+  color: #b3adad;
+}
 
-    .textCompleted {
-        text-decoration: line-through;
-        color: #b3adad;
-    }
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
 
-    .removeBtn {
-        margin-left: auto;
-        color: #de4343;
-    }
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
 </style>
