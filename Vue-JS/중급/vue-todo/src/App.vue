df9c3a673d5cad4a509b02fa792433f3735267cf
hyena0608 <goatsfish@gmail.com>
2022-04-24 13:47:02 +0900

    [Vue] todolist - feat: 컴포넌트 할일 저장 기능 구현

--- Vue-JS/중급/vue-todo/src/App.vue
+++ Vue-JS/중급/vue-todo/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <TodoHeader></TodoHeader>
     <TodoInput></TodoInput>
     <TodoList></TodoList>
@@ -8,20 +8,37 @@
 </template>
 
 <script>
-import TodoHeader from "./components/TodoHeader.vue";
-import TodoInput from "./components/TodoInput.vue";
-import TodoList from "./components/TodoList.vue";
-import TodoFooter from "./components/TodoFooter.vue";
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
 
 export default {
   components: {
-    TodoHeader: TodoHeader,
-    TodoList: TodoList,
-    TodoInput: TodoInput,
-    TodoFooter: TodoFooter,
-  },
-};
+    'TodoHeader': TodoHeader,
+    'TodoList': TodoList,
+    'TodoInput': TodoInput,
+    'TodoFooter': TodoFooter
+  }
+}
 </script>
 
 <style>
+body {
+  text-align: center;
+  background-color: F6F6F6;
+}
+
+input {
+  border-style: groove;
+  width: 200px;
+}
+
+button {
+  boder-style: groove;
+}
+
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
 </style>
