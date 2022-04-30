4bfbded6c2adff42bf5c45d864f9b778dad05278
hyena0608 <goatsfish@gmail.com>
2022-04-30 17:03:51 +0900

    [멋쟁이사자처럼 : Django] simplesns - feat : Board 모델 추가

---
+++ 멋쟁이사자처럼/운영진-세션/django-board-2/simplesns/snsapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Board(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=64)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    
+    def __str__(self):
+        return self.title
