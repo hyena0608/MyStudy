99bbb0f16999b47a0a54aa4e4dbf68494899621e
hyena0608 <goatsfish@gmail.com>
2022-06-18 08:05:27 +0900

    [Servlet Programming] : Servlet 생명주기 알아보기

---
+++ 스프링/servelt-programming/src/main/java/com/hyena/serveltprogramming/servlets/HelloWorld.java
@@ -0,0 +1,36 @@
+package com.hyena.serveltprogramming.servlets;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class HelloWorld implements Servlet {
+    ServletConfig config;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("init() 호출됨");
+        this.config = config;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        System.out.println("getServletConfig() 호출됨");
+        return this.config;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("service() 호출됨");
+    }
+
+    @Override
+    public String getServletInfo() {
+        System.out.println("getServletInfo() 호출됨");
+        return "version=1.0;author=parkhyunseo;copyright=parkhyunseo 2022";
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy() 호출됨");
+    }
+}
