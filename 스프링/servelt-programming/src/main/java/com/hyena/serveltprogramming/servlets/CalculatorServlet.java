88a0221ccc307909a2faabc2c90d2e0ba7a6903e
hyena0608 <goatsfish@gmail.com>
2022-06-18 09:32:46 +0900

    [Servlet Programming] web.xml 매핑이 아닌 @WebServlet 어노테이션 사용하기

--- 스프링/servelt-programming/src/main/java/com/hyena/serveltprogramming/servlets/CalculatorServlet.java
+++ 스프링/servelt-programming/src/main/java/com/hyena/serveltprogramming/servlets/CalculatorServlet.java
@@ -4,9 +4,11 @@ import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/calc")
 public class CalculatorServlet extends GenericServlet {
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
