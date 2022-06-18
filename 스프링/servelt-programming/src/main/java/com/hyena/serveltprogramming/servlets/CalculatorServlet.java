200e17b784a405b6bc0c1e2316c5b3948eadc364
hyena0608 <goatsfish@gmail.com>
2022-06-18 09:17:58 +0900

    [Servlet Programming] CalculateServlet 배치 테스트

---
+++ 스프링/servelt-programming/src/main/java/com/hyena/serveltprogramming/servlets/CalculatorServlet.java
@@ -0,0 +1,27 @@
+package com.hyena.serveltprogramming.servlets;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CalculatorServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        int a = Integer.parseInt(req.getParameter("a"));
+        int b = Integer.parseInt(req.getParameter("b"));
+
+        res.setContentType("text/plain");
+        res.setCharacterEncoding("UTF-8");
+        PrintWriter writer = res.getWriter();
+        writer.println("a = " + a + ", " + "b = " + b + "의 계산 결과 입니다.");
+        writer.println("a + b = " + (a + b));
+        writer.println("a - b = " + (a - b));
+        writer.println("a * b = " + (a * b));
+        writer.println("a / b = " + ((float) a / (float) b));
+        writer.println("a % b = " + (a % b));
+
+    }
+}
