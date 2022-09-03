20c88fc70316cb6b5e69fc426e790d4359d9338f
hyena0608 <goatsfish@gmail.com>
2022-09-03 19:25:33 +0900

    [Spring JPA original] 연습

--- 스프링/jpa-programming/jpabook/src/main/java/jpashop/domain/Order.java
+++ 스프링/jpa-programming/jpabook/src/main/java/jpashop/domain/Order.java
@@ -4,6 +4,7 @@ package jpashop.domain;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -21,13 +22,30 @@ public class Order {
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    private LocalDateTime orderDate;
+    @OneToOne
+    @JoinColumn(name = "DELIVERY_ID")
+    private Delivery delivery;
+
+    private Date orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    public void setMember(Member member) {
+        if (this.member != null) {
+            this.member.getOrders().remove(this);
+        }
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
     public void addOrderItem(OrderItem orderItem) {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
     }
 }
