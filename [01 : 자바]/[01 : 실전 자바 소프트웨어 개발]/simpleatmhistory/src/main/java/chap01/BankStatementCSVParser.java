88ff89d06454aa559ce1a2b3b45e01175a3f26c8
hyena0608 <goatsfish@gmail.com>
2022-10-14 21:16:35 +0900

    feat : CSV 파싱 클래스 구현

---
+++ [01 : 자바]/[01 : 실전 자바 소프트웨어 개발]/simpleatmhistory/src/main/java/chap01/BankStatementCSVParser.java
@@ -0,0 +1,29 @@
+package chap01;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * 예제 2-3
+ * 파싱 로직을 추출해 클래스로 만들기
+ *
+ * 이전 BankTransactionAnalyzerProblem 클래스는 CSV만 파싱할 수 있도록 하드코딩 되어 있다.
+ * 현재 클래스는 SRP를 지키기 위해서 작성한다.
+ * 바로 CSV 파싱 로직을 새로운 클래스로 분리하는 것이다.
+ */
+public class BankStatementCSVParser {
+
+    private static final DateTimeFormatter DATE_PATTERN
+            = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    private BankTransaction parseFromCSV(final String line) {
+        final String[] columns = line.split(",");
+
+        final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+        final double amount = Double.parseDouble(columns[1]);
+        final String description = columns[2];
+
+        return new BankTransaction(date, amount, description);
+    }
+
+}
