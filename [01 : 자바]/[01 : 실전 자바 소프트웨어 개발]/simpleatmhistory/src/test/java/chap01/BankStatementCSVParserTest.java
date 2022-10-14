04aaad264b108f80e9595a3900757b2e4b9ee89f
hyena0608 <goatsfish@gmail.com>
2022-10-15 07:19:18 +0900

    add : 예제 3-1, 3-2, 3-3 특정 범위 조건 검색 기능

---
+++ [01 : 자바]/[01 : 실전 자바 소프트웨어 개발]/simpleatmhistory/src/test/java/chap01/BankStatementCSVParserTest.java
@@ -0,0 +1,31 @@
+package chap01;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+/**
+ * 예제 2-15
+ * 어서션 구문 사용하기
+ */
+class BankStatementCSVParserTest {
+
+    private final BankStatementParser statementParser = new BankStatementCSVParser();
+
+    @Test
+    void shouldParseOneCorrectLine() {
+        final String line = "30-01-2017,-50,Tesco";
+
+        final BankTransaction result = statementParser.parseFrom(line);
+
+        final BankTransaction expected
+                = new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
+        final double tolerance = 0.0d;
+
+        Assertions.assertEquals(expected.getDate(), result.getDate());
+        Assertions.assertEquals(expected.getAmount(), result.getAmount(), tolerance);
+        Assertions.assertEquals(expected.getDescription(), result.getDescription());
+    }
+}
