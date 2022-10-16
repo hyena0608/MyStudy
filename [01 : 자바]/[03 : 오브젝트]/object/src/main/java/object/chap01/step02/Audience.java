522b8f5bf75e7f23fe3eb3b0870f1202787e4a59
hyena0608 <goatsfish@gmail.com>
2022-10-17 07:52:21 +0900

    [Object] chap01-step02 : 내부 구현의 캡슐화, 객체의 자율성

--- [01 : 자바]/[03 : 오브젝트]/object/src/main/java/object/chap01/step02/Audience.java
+++ [01 : 자바]/[03 : 오브젝트]/object/src/main/java/object/chap01/step02/Audience.java
@@ -8,7 +8,18 @@ public class Audience {
         this.bag = bag;
     }
 
-    public Bag getBag() {
-        return bag;
+    /**
+     * Bag의 존재 캡슐화
+     * (기존 : TicketSeller가 직접 Bag에 접근했었다.)
+     */
+    public Long buy(Ticket ticket) {
+        if (bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
     }
 }
--- [01 : 자바]/[03 : 오브젝트]/object/src/main/java/object/chap01/step02/TicketSeller.java
+++ [01 : 자바]/[03 : 오브젝트]/object/src/main/java/object/chap01/step02/TicketSeller.java
@@ -8,15 +8,12 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
+    /**
+     * TicketSeller 캡슐화
+     * TicketOffice에 대한 접근은 오직 TicketSeller 안에만 존재하게 된다.
+     * (기존 : Theater에서 TicketOffice 접근 가능했었다.)
+     */
     public void sellTo(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketOffice.plusAmount(ticketOffice.getTicket().getFee());
     }
 }
