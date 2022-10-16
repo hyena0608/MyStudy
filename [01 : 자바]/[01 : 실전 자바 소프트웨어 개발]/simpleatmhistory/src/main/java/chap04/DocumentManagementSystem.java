334d39e897bde4d4d7a1b9896ce36d7392670ac1
hyena0608 <goatsfish@gmail.com>
2022-10-16 13:25:37 +0900

    add : 예제 4-7 선행조건

--- [01 : 자바]/[01 : 실전 자바 소프트웨어 개발]/simpleatmhistory/src/main/java/chap04/DocumentManagementSystem.java
+++ [01 : 자바]/[01 : 실전 자바 소프트웨어 개발]/simpleatmhistory/src/main/java/chap04/DocumentManagementSystem.java
@@ -1,6 +1,11 @@
 package chap04;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -8,14 +13,40 @@ import java.util.Map;
  * - 임포터 등록
  * 예제 4-7
  * - importFile 정의
+ *  - 검증 메서드
  */
 public class DocumentManagementSystem {
 
+    private final List<Document> documents = new ArrayList<>();
     private final Map<String, Importer> extensionToImporter = new HashMap<>();
 
     public DocumentManagementSystem() {
         extensionToImporter.put("letter", new LetterImporter());
         extensionToImporter.put("report", new ReportImporter());
         extensionToImporter.put("jpg", new ImageImporter());
+    }
+
+    public void importFile(final String path) throws IOException {
+        final File file = new File(path);
+        if (!file.exists()) {
+            throw new FileNotFoundException(path);
+        }
+
+        final int separatorIndex = path.lastIndexOf('.');
+        if (separatorIndex != -1) {
+            if (separatorIndex == path.length()) {
+                throw new UnknownFileTypeException("No extension found For file: " + path);
+            }
+            final String extension = path.substring(separatorIndex + 1);
+            final Importer importer = extensionToImporter.get(extension);
+            if (importer == null) {
+                throw new UnknownFileTypeException("for File: " + path);
+            }
+
+            final Document document = importer.importFile(file);
+            documents.add(document);
+        } else {
+            throw new UnknownFileTypeException("No extension found For file: " + path);
+        }
     }
 }
---
+++ [01 : 자바]/[01 : 실전 자바 소프트웨어 개발]/simpleatmhistory/src/main/java/chap04/UnknownFileTypeException.java
@@ -0,0 +1,8 @@
+package chap04;
+
+public class UnknownFileTypeException extends RuntimeException {
+
+    public UnknownFileTypeException(final String message) {
+        super(message);
+    }
+}
