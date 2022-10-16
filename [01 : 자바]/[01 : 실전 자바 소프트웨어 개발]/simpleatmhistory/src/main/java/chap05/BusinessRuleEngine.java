a48af1ee51461a7ce34b444da579d1a87448ea24
hyena0608 <goatsfish@gmail.com>
2022-10-16 15:42:59 +0900

    add : 예제 5-4 비즈니스 규칙 엔진 기본 구현

--- [01 : 자바]/[01 : 실전 자바 소프트웨어 개발]/simpleatmhistory/src/main/java/chap05/BusinessRuleEngine.java
+++ [01 : 자바]/[01 : 실전 자바 소프트웨어 개발]/simpleatmhistory/src/main/java/chap05/BusinessRuleEngine.java
@@ -1,17 +1,28 @@
 package chap05;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 예제 5-1
  * 비즈니스 규칙 엔진의 기본 API
+ * 예제 5-4
+ * 비즈니스 규칙 엔진 기본 구현
  */
 public class BusinessRuleEngine {
 
+    private final List<Action> actions;
+
+    public BusinessRuleEngine() {
+        this.actions = new ArrayList<>();
+    }
+
     public void addAction(final Action action) {
-        throw new UnsupportedOperationException();
+        this.actions.add(action);
     }
 
     public int count() {
-        throw new UnsupportedOperationException();
+        return this.actions.size();
     }
 
     public void run() {
