9952bb0c668906b3d1f2fd1a53dbb2d51679b09a
hyena0608 <goatsfish@gmail.com>
2022-10-17 16:32:46 +0900

    [Object] chap02-step02 : 추상화를 이용한 상위 정책 기술로 쉬운 기능 추가

---
+++ [01 : 자바]/[03 : 오브젝트]/object/src/main/java/object/chap02/step02/NoneDiscountPolicy.java
@@ -0,0 +1,18 @@
+package object.chap02.step02;
+
+import object.chap02.step01.DiscountPolicy;
+import object.chap02.step01.Money;
+import object.chap02.step01.Screening;
+
+/**
+ * Movie와 DiscountPolicy는 수정하지 않고
+ * NoneDiscountPolicy라는 새로운 클래스를 추가하는 것만으로
+ * 애플리케이션의 기능을 확장했다.
+ */
+public class NoneDiscountPolicy extends DiscountPolicy {
+
+    @Override
+    protected Money getDiscountAmount(Screening screening) {
+        return Money.ZERO;
+    }
+}
