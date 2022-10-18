bcb47a4fe253fed9f5db145f80c0c8e690530c7d
hyena0608 <goatsfish@gmail.com>
2022-10-18 15:03:44 +0900

    [Dev-Conquer] :  기능을 막았지만 LSP 위반, 확장에 열려있지 않다

---
+++ [01 : 자바]/[04 : 개발자가 반드시 정복]/dev-conquer/src/main/java/chap05/step03/CantSupportSquareException.java
@@ -0,0 +1,7 @@
+package chap05.step03;
+
+public class CantSupportSquareException extends RuntimeException {
+
+    public CantSupportSquareException() {
+    }
+}
--- [01 : 자바]/[04 : 개발자가 반드시 정복]/dev-conquer/src/main/java/chap05/step03/Rectangle.java
+++ [01 : 자바]/[04 : 개발자가 반드시 정복]/dev-conquer/src/main/java/chap05/step03/Rectangle.java
@@ -5,7 +5,24 @@ public class Rectangle {
     private int width;
     private int height;
 
+    /**
+     * LSP 위반
+     */
     public void increaseHeight(Rectangle rec) {
+        if (rec.getHeight() <= rec.getWidth()) {
+            rec.setHeight(rec.getWidth() + 10);
+        }
+    }
+
+    /**
+     * 실제 타입이 Square일 경우 기능 사용 X
+     * 하지만 instanceof 연산자 사용 -> LSP 위반
+     * increaseHeight 메서드는 Rectange의 확장에 열려있지 않다.
+     */
+    public void increaseHeight_2(Rectangle rec) {
+        if (rec instanceof Square) {
+            throw new CantSupportSquareException();
+        }
         if (rec.getHeight() <= rec.getWidth()) {
             rec.setHeight(rec.getWidth() + 10);
         }
