a325e401b8501b9281ab8a3989aa7141909a98df
hyena0608 <goatsfish@gmail.com>
2022-10-18 14:58:56 +0900

    [Dev-Conquer] : 자식 클래스 들어올 시 LSP 위반

--- [01 : 자바]/[04 : 개발자가 반드시 정복]/dev-conquer/src/main/java/chap05/step03/Rectangle.java
+++ [01 : 자바]/[04 : 개발자가 반드시 정복]/dev-conquer/src/main/java/chap05/step03/Rectangle.java
@@ -5,6 +5,12 @@ public class Rectangle {
     private int width;
     private int height;
 
+    public void increaseHeight(Rectangle rec) {
+        if (rec.getHeight() <= rec.getWidth()) {
+            rec.setHeight(rec.getWidth() + 10);
+        }
+    }
+
     public int getWidth() {
         return width;
     }
