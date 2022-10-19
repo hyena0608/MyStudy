cfc12572d4fcc7301cfcd3db2320c0ff6420d5aa
hyena0608 <goatsfish@gmail.com>
2022-10-19 12:33:36 +0900

    [Object] chap04-step02 : 낮은 응집도, 내부 구현이 인터페이스에 노출된 곳에 직접적으로 의존

--- [01 : 자바]/[03 : 오브젝트]/object/src/main/java/object/chap04/step02/Screening.java
+++ [01 : 자바]/[03 : 오브젝트]/object/src/main/java/object/chap04/step02/Screening.java
@@ -8,6 +8,14 @@ public class Screening {
     private int sequence;
     private LocalDateTime whenScreened;
 
+    /**
+     * DiscountCondition과 Movie의 내부 구현이 인터페이스에 노출
+     * -> Screening은 노출된 구현에 직접적으로 의존
+     * -> 낮은 응집도
+     * DiscountCondition 판단 정보 변경 시
+     * Movie의 isDiscountable 파라미터 종류가 변경되고
+     * Screening에서 Movie를 호출하는 isDiscountable 메서드도 변경되야 한다.
+     */
     public Money calculateFee(int audienceCount) {
         switch (movie.getMovieType()) {
             case AMOUNT_DISCOUNT:
